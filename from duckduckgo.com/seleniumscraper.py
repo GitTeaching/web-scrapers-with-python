02707018df39b07a27e65a17a6525c3b9c262612
GitTeaching <aid_aicha@hotmail.fr>
2020-05-08 14:07:27 +0100

    Add files via upload

---
+++ from duckduckgo.com/seleniumscraper.py
@@ -0,0 +1,16 @@
+from selenium.webdriver import Chrome
+
+
+browser = Chrome('D:/Python ML DL/Projects/web-scrapers/chromedriver_win32/chromedriver.exe')
+
+browser.get('https://duckduckgo.com')
+
+search_form = browser.find_element_by_id('search_form_input_homepage')
+search_form.send_keys('python')
+search_form.submit()
+
+results = browser.find_elements_by_class_name('result')
+print(results[0].text)
+
+browser.close()
+quit()
---
+++ from youtube.com/seleniumscraper.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import pandas as pd
+
+url = 'https://www.youtube.com/channel/UC8tgRQ7DOzAbn9L7zDL8mLg/videos?view=0&sort=p&flow=grid'
+
+browser = webdriver.Chrome('D:/Python ML DL/Projects/web-scrapers/chromedriver_win32/chromedriver.exe')
+browser.get(url)
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight)") 
+
+videos = browser.find_elements_by_class_name('style-scope ytd-grid-video-renderer')
+
+videos_list = []
+
+for video in videos:
+	title = video.find_element_by_xpath('.//*[@id="video-title"]').text
+	views = video.find_element_by_xpath('.//*[@id="metadata-line"]/span[1]').text
+	when = video.find_element_by_xpath('.//*[@id="metadata-line"]/span[2]').text
+
+	vid_item = {
+	    'title': title,
+	    'views': views,
+	    'posted': when
+	}
+
+	videos_list.append(vid_item)
+
+
+df = pd.DataFrame(videos_list)
+print(df)
